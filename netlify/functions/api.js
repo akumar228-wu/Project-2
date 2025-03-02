abb92b280df24c1e444c42ed9dee591fb0d3a211
ame <a.kumar.228@westcliff.edu>
2025-03-02 04:42:51 -0800

    testing without api route

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -157,7 +157,7 @@ app.use((req, res, next) => {
 //    ROUTES
 //=======================
 // 8. Routes
-app.use('/api/', routes);
+app.use('/', routes);
 
 //=======================
 //    ERROR HANDLING
